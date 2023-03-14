9aa63ec1e18e94c99a1624940a71ea3e7339acca
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-14 16:43:44 +0300

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require "pry"
+class Anagram 
+    attr_reader :word
+    attr_reader :arr
+    def initialize(word)
+        @word = word
+        
+    end
+    def match(arr) 
+        result =[]
+        arr.each do |b|
+            if word.chars.sort.join == b.split('').sort.join
+            
+            result << b 
+                       end
+                     end
+            result
+    end
+
+end
+w = Anagram.new("listen")
+w.match(%w[enlists google inlets banana])
+
+
